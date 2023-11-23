76d2f6ddf066e1fbc7a0e8663f57b28b3b7dd3a4
Maksim Dubinin <148623996+MaksimDubinin@users.noreply.github.com>
2023-11-23 19:25:53 +0300

    Add files via upload

---
+++ src/main/resource/insert.sql
@@ -0,0 +1,7 @@
+insert into audit_department
+(revisinNumber, productType, adress)
+values
+    ('Xg_5124', 'Компьютер', 'Общага'),
+    ('Du_2694', 'Клавиатура', 'Дубна'),
+    ('Hq_1033', 'Монитор', 'Оршанка'),
+    ('Qp_1784','Процессор','DNS');
